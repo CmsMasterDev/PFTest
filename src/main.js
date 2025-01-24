80ccae6c0c70acde9a6d5230140c0e1a78908ec2
dev_ <>
2025-01-24 16:55:44 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { registerElement } from './registerElements';
 
 // import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
-
 // Day.Js
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone.js'
@@ -16,30 +15,8 @@ window.dayjs = dayjs
 
 
 import App from './components/App.vue'
-
 import Widget from './components/Widget.vue'
-
-// Vue Instance
-const app = createApp({
-  data() {
-      return {
-        loading: true,
-        routerKey:'',
-        title:''
-      }
-    }
-})
-
 
 
 registerElement('main-element',App)
 registerElement('example-widget',Widget)
-
-
-
-
-// const { t } = useI18n({ i18n });
-
-// const app = createApp(App)
-// app.use(createPinia())
-// app.mount('#app')
